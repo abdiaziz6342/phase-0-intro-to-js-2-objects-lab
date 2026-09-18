25cf5fb94956da7ed44c66b790fc1f1c464428f1
Abdiaziz Adan <abdiazizadan344@gmail.com>
2026-09-18 15:52:15 +0300

    objects-lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+let employee = {
+    name: 'abdi',
+    streetAddress: '11 Broadway',
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+   return { ...employee,
+    [key]: value,
+    };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const clone = {...employee};
+    delete clone[key];
+    return clone;
+} 
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
